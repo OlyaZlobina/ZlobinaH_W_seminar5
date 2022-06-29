416c037e9cf67da78995897c25d15877e7689d31
Olya <Zlobinaolya87@gmail.com>
2022-06-29 09:07:41 +0300

    first task done

--- homeTasks/Program.cs
+++ homeTasks/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] positivNumbersArray(int size)
+{
+    int[] Array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Array[i] = new Random().Next(100, 999);
+    }
+    return Array;
+}
+
+void evenNumbers(int[] Size)
+{
+    int count = 0;
+    for (int i = 0; i < Size.Length; i++)
+    {
+        if(Size[i] % 2==0)
+        {
+            count++;
+        }
+    }
+    Console.Write($" -> {count}");
+}
+
+void PrintPositivNumbersArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int[] Arr = positivNumbersArray(8);
+PrintPositivNumbersArray(Arr);
+evenNumbers(Arr);
+Console.WriteLine();
